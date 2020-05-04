d9cdf602aa0e58877e3fa8c21fddf013516f9ec8
karifl <lampi_88@hotmail.com>
2020-05-04 20:27:58 +0300

    Delete Memory.java

--- Memory.java
+++
@@ -1,8 +0,0 @@
-package facade;
-
-public class Memory {
-	public void load(long position, byte[] data) {
-		String x = new String(data);
-		System.out.println("RAM: Loading data from BOOT SECTOR "+x +" from BOOT ADDRESS " + position + " to memory");
-	}
-}
