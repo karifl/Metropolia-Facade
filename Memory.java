d9dbc60ac753a4df36192a3cc886f1ee262a0d1b
karifl <lampi_88@hotmail.com>
2020-05-04 23:18:15 +0300

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -4,7 +4,7 @@ public class Memory {
 	public void load(long position, byte[] data) {
 		System.out.print("RAM: Loading data from BOOT SECTOR ");
 		for (byte pointer : data) {
-			System.out.print("[" +pointer +"]");
+			System.out.print("[" +(char) pointer +"]");
 		}
 		System.out.println(" from BOOT ADDRESS " + position + " to memory");
 	}
